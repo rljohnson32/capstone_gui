2f2a144feef2f5c45f37e3435981293d8572a5d5
Robert Johnson <rljohnson32@wisc.edu>
2018-04-10 17:24:24 -0500

    Styling for weather widget is now beauuuuutiful, matches apple weather widget almost exactly, need to finish js code to populate the new weather table'

--- js/newWeather.js
+++ js/newWeather.js
@@ -5,7 +5,7 @@ var curLocReq = 0;
 var zip = 0;
 var city = '';
 var state = '';
-var baseURLw = "http://api.wunderground.com/api/4a44ed522a5071aa/conditions/forecast10day/q/"
+var baseURLw = "http://api.wunderground.com/api/4a44ed522a5071aa/conditions/hourly/forecast10day/q/"
 var query;
 var URL;
 var madisonURL = "http://api.wunderground.com/api/4a44ed522a5071aa/conditions/forecast10day/q/WI/Madison.json"
@@ -37,7 +37,7 @@ function updateWeather(){
       query = state + '/' + city;
     }
     else{
-      query = 96801;
+      query = 53029;
     }
   }
   else{
@@ -56,53 +56,99 @@ function updateWeather(){
           curCity = data.current_observation.display_location.city;
           var temp_f = data.current_observation.temp_f.toString().substring(0,2) + '&deg;'+' F';
           curTemp = data.current_observation.temp_f.toString().substring(0,2);
-          var feelsLike = data.current_observation.feelslike_string.substring(0,2) + '&deg;'+' F';
+          // var feelsLike = data.current_observation.feelslike_string.substring(0,2) + '&deg;'+' F';
           //var desc = data.current_observation.icon;
           //var weatherDescription = desc.charAt(0).toUpperCase() + desc.slice(1);
           var weatherDescription = data.current_observation.weather;
           curCond = weatherDescription;
           var icon = data.current_observation.icon_url;
+          //<img src="http://icons.wxug.com/i/c/k/partlycloudy.gif">
           var forecast = data.forecast.simpleforecast.forecastday;
-          var rainChance = forecast[0].pop;
+          var hourlyforecast = data.hourly_forecast;
+          var rainChance = forecast[0].pop + '%';
           rainPercent = rainChance + " percent";
 
-          //alert("Current temperature in " + location + " is: " + temp_f);
-          html = '<h2>'+location+'</h2>';
+          $("#weatherLocation").html(location);
+          $("#curWeatherSymbol").html('<img src="' + icon + '">');
+          $("#curTempF").html(temp_f);
 
-          html += '<div id="currentWeatherList">';
-          html += '<ul>';
-          html += '<li class="currently">'+weatherDescription+' <img src=' + icon +'></li>';
-          html += '<li>Current Temp: '+temp_f+'</li>';
-          html += '<li>Feels Like: '+feelsLike+'</li>';
-          html += '<li>Chance of Rain: '+rainChance+'%</li></ul>';
-          html += '</div>';
-          // html += '<input id="speak" type="submit" onclick="sayCurrentWeather()" value="Speak Weather"></br>';
+          $("#nowR").html(rainChance);
+          $("#nowS").html('<img src="' + icon + '">');
+          $("#nowT").html(curTemp);
 
-          html += '<i style="font-size:15px;color:grey">Updated: <span id="lastWeatherUpdate">'+ lastTimeWeatherUpdated +'</span></i></br></br>';
-          //html += '<img src=' + icon +'></br>';
+          var hour;
+          var pop;
+          var symbol;
+          var hourlytemp;
 
-          html += '<div id="weatherForm" style="display: none;">';
-          html += 'Zip Code:  </br><input id="zip" size="6" maxlength="5">';
-          html += '<input type="submit" onclick="getZip()" value="Update"></br>';
-          html += 'City:  </br><input type="text" id="city" size="14"></br>';
-          html += 'State:  </br><input type="text" id="state" size="14">';
-          html += '<input type="submit" onclick="getCity()" value="Update"></br>';
-          html += '<button onclick="getLocation()" >Current Location</button>';
-          html += '</div>'
-          html += '<input id="toggler" type="submit" onclick="toggleForm()" value="Change"></br>';
+          // for(i = 1; i < 6; i++){
+          //   hour = 
+          //   pop = 
+          //   symbol = 
+          //   hourlytemp = 
+          //   $("#" + i + 'hr').html(hour);
+          //   $("#" + i + 'hrR').html(pop);
+          //   $("#" + i + 'hrS').html('<img src="' + symbol + '">');
+          //   $("#" + i + 'hrT').html(hourlytemp);
+          // }
 
-          html += '<h2>5 Day Forecast</h2>';
-          html += '<ul>';
+          var day;
+          var daySymbol;
+          var daypop;
+          var dayHigh;
+          var dayLow = '  '; //need to put 2 spaces on the front of this string, see line 200 in gui.html
+
+          // for(i = 1; i < 6; i++){
+          //   day = 
+          //   daySymbol = 
+          //   daypop = 
+          //   dayHigh = 
+          //   dayLow =
+          //   $("#day" + i).html(day);
+          //   $("#day" + i + 'S').html('<img src="' + daySymbol + '">');
+          //   $("#day" + i + 'R').html(daypop);
+          //   $("#day" + i + 'H').html(dayHigh);
+          //   $("#day" + i + 'L').html(dayLow);
+          // }
+
+          // //alert("Current temperature in " + location + " is: " + temp_f);
+          // html = '<h2>'+location+'</h2>';
+
+          // html += '<div id="currentWeatherList">';
+          // html += '<ul>';
+          // html += '<li class="currently">'+weatherDescription+' <img src=' + icon +'></li>';
+          // html += '<li>Current Temp: '+temp_f+'</li>';
+          // html += '<li>Feels Like: '+feelsLike+'</li>';
+          // html += '<li>Chance of Rain: '+rainChance+'%</li></ul>';
+          // html += '</div>';
+          // // html += '<input id="speak" type="submit" onclick="sayCurrentWeather()" value="Speak Weather"></br>';
+
+          // html += '<i style="font-size:15px;color:grey">Updated: <span id="lastWeatherUpdate">'+ lastTimeWeatherUpdated +'</span></i></br></br>';
+          // //html += '<img src=' + icon +'></br>';
+
+          // html += '<div id="weatherForm" style="display: none;">';
+          // html += 'Zip Code:  </br><input id="zip" size="6" maxlength="5">';
+          // html += '<input type="submit" onclick="getZip()" value="Update"></br>';
+          // html += 'City:  </br><input type="text" id="city" size="14"></br>';
+          // html += 'State:  </br><input type="text" id="state" size="14">';
+          // html += '<input type="submit" onclick="getCity()" value="Update"></br>';
+          // html += '<button onclick="getLocation()" >Current Location</button>';
+          // html += '</div>'
+          // html += '<input id="toggler" type="submit" onclick="toggleForm()" value="Change"></br>';
+
+          // html += '<h2>5 Day Forecast</h2>';
+          // html += '<ul>';
           
-          for (i = 1; i < 6; i++) { 
-            html += '<li><b>'+forecast[i].date.weekday_short+ ':<img src=' + forecast[i].icon_url +'></b></li>';
-            html += forecast[i].conditions+'</br>';
-            html += 'High Temp: ' + forecast[i].high.fahrenheit + '&deg;'+' F</br>';
-            html += 'Chance of Rain: '+forecast[i].pop+'%</br>';
-          }
-          html += '</ul>';
-          html += '<p>Weather updated every ' + refreshRate+ ' minutes</p>';
-       $("#weather").html(html);
+          // for (i = 1; i < 6; i++) { 
+          //   html += '<li><b>'+forecast[i].date.weekday_short+ ':<img src=' + forecast[i].icon_url +'></b></li>';
+          //   html += forecast[i].conditions+'</br>';
+          //   html += 'High Temp: ' + forecast[i].high.fahrenheit + '&deg;'+' F</br>';
+          //   html += 'Chance of Rain: '+forecast[i].pop+'%</br>';
+          // }
+          // html += '</ul>';
+          // html += '<p>Weather updated every ' + refreshRate+ ' minutes</p>';
+
+       // $("#weather").html(html);
        })
        .catch(function(err) {
         console.log('ERROR FETCHING WEATHER DATA:', err);
@@ -116,6 +162,16 @@ function updateWeather(){
         $("#weather").html(html);
     });
   updateLastUpdate();
+}
+
+function toggleWeatherForm(){
+  var x = document.getElementById("weatherForm");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+      // document.getElementById("newstock").focus();
+  } else {
+      x.style.display = "none";
+  }
 }
 
 function sayCurrentWeather(){
@@ -138,6 +194,7 @@ function updateLastUpdate(){
   if (hour   == 0) { hour = 12;             }
   if (minute < 10) { minute = "0" + minute; }
   lastTimeWeatherUpdated = hour + ':' + minute + ' ' + ap;
+  $('#lastWeatherUpdate').html(lastTimeWeatherUpdated);
   //$('#lastWeatherUpdate').html(hour + ':' + minute + ' ' + ap);
   //alert('updated with' + hour + ':' + minute + ' ' + ap);
   //alert($('#lastWeatherUpdate').html());
